3729d546e8e681c56ddd79bd638a51848ea12e48
Mathieu Roberts <mathieulroberts@gmail.com>
2022-09-05 18:31:29 -0700

    made buttons!

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
 'use strict';
+
+function buttonClick () {
+    console.log('button Clicked!!');
+};
+
+let easyButtonId = document.getElementById('easy');
+let mediumButtonId = document.getElementById('medium');
+let hardButtonId = document.getElementById('hard');
+let resetButtonId = document.getElementById('reset');
+const keyboardButtons = document.getElementById('keyboardButtons');
+
+easyButtonId.addEventListener('click', buttonClick);
+mediumButtonId.addEventListener('click', buttonClick);
+hardButtonId.addEventListener('click', buttonClick);
+resetButtonId.addEventListener('click', buttonClick);
+
+function generateButton() {
+    const buttonsHTML = "abcdefghijklmnopqrstuvwxyz.-"
+      .split("")
+      .map(
+        (letter) =>
+          `<button
+           class = "alphabetButtonJS" 
+           id="${letter}"
+           >
+          ${letter}
+          </button>`
+      )
+      .join("");
+  
+    return buttonsHTML;
+  }
+
+keyboardButtons.innerHTML = generateButton();
+
+
+
+
+
+
+
+
+
+
+
+
